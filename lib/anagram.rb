0c673dae830fb4c4902bc72dd51e7abc352e11a7
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-01 11:28:56 -0400

    commit tree b25dec601e9acc6e2ecaf75236b40b3d826583bd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.include?(words)
+    end
+end
